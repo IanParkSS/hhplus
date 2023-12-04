64dd5fde050be288976161088599cf4abce325de
박상훈 <ian@bagsanghun-ui-MacBookAir.local>
2023-12-04 11:02:39 +0900

    first commit

---
+++ src/test/java/com/example/hhplus/HhplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hhplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
